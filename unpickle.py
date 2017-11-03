ec4ac3f76b9046af751d282407848497a863b315
csc103 student <me@citymail.ccny.cuny.edu>
2017-11-03 15:27:10 -0400

    Added function to aggregate training data

--- unpickle.py
+++ unpickle.py
@@ -1,41 +1,67 @@
-import cPickle
+import _pickle as cPickle
 import os 
 import sys
+import numpy as np
 
 def do_unpickle(file_name):
 	with open('./cifar-10-batches-py/'+file_name, 'rb') as to_open:
-		dict = cPickle.load(to_open)
+            dict = cPickle.load(to_open, encoding = 'bytes')
 	return dict
 
-if __name__ == "__main__":
-	data = {}
-	files = os.listdir('./cifar-10-batches-py')
-	file_count = 0;
-	for file_index in range(len(files)):
-		if(str(files[file_index]).startswith('data_batch')):
-			file_count = file_count+1	
-			data["batch_{}".format(str(file_count))] = do_unpickle(files[file_index])	
+def get_hyperparameters():
+    data = {}
+    files = os.listdir('./cifar-10-batches-py')
+    file_count = 0;
+    for file_index in range(len(files)):
+        if(str(files[file_index]).startswith('data_batch')):
+            file_count = file_count + 1	
+            data["batch_{}".format(str(file_count))] = do_unpickle(files[file_index])
+    
+    # print(data['batch_1'][b'data'].shape)
+    #print(data.keys())
 
-	# instructions on how the script works
-	with open('./README_UNPICKLE', 'wb') as f:
-		f.write('Reaid CIFAR-10 database into dictionary\n')
-		f.write('The keys of the dictionary are\n\n')
-                for item in data.keys():
-                    if(str(item) != 'batch_5'):
-                        f.write(str(item) + ' , ')
-                    else:
-                        f.write(str(item))
-                f.write('\n')
-		#f.write(data.keys())
-		f.write('\nWithin each key is another dictionary that holds the following values for each data batch:\n\n')
-                for item in data['batch_1'].keys():
-                    if(str(item) != 'filenames'):
-                        f.write(str(item) + ' , ')
-                    else:
-                        f.write(str(item))
-                f.write('\n')
-		#f.write(data['batch_1'].keys())
-		f.write('\nThe data key holds a numpy.ndarray of 8 bit unsigned ints indicated RGB values of a 32x32 image\n')
-		f.write('Here is some sample data:')
-		#f.write(data['batch_1']['data'])	
+    # The code above this point unwraps the binary data into ndarrays stored within dictionaries.
+    # We may now aggregate all the ndarray's for training into a simple ndarray
+    
+    for batch in data.keys():
+        #print('in ' + str(batch));
+        #print('size of its data is ' + str(data[batch][b'data'].shape))
+        if(batch == 'batch_1'):
+            X_train = np.array(data[batch][b'data'])
+            Y_train = np.array(data[batch][b'labels'])
+            Y_train = Y_train.reshape((Y_train.shape[0], 1)) 
+            #print('size of its labels is ' + str(Y_train.shape))
+        else:
+            X_train_segment = np.array(data[batch][b'data'])
+            Y_train_segment = np.array(data[batch][b'labels'])
+            Y_train_segment = Y_train_segment.reshape((Y_train_segment.shape[0], 1))
+            X_train = np.append(X_train, X_train_segment, axis = 0)
+            Y_train = np.append(Y_train, Y_train_segment, axis = 0)
+            #print('size of its labels is ' + str(Y_train_segment.shape))
 
+    return X_train, Y_train
+    #print('the total size of X_train is now ' + str(X_train.shape))
+    #print('the total size of Y_train is now ' + str(Y_train.shape))
+
+if __name__ == '__main__':
+    X, Y = get_hyperparameters();
+    print(X.shape)
+    print(Y.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+      
